187b34a431848310f279b7f5f090baa40c2dfd3e
Sergey Volkov <sergey.volkov.freelancer@gmail.com>
2022-10-04 00:05:38 +0300

    Complete animation option

--- src/App.js
+++ src/App.js
@@ -1,6 +1,25 @@
 import React, { useState } from "react";
 import "./index.scss";
 
+const Modal = ({ open, setOpen, children }) => (
+  <div className={`overlay animated ${open ? "show" : ""}`}>
+    <div className="modal">
+      <svg
+        onClick={() => {
+          setOpen(false);
+        }}
+        height="200"
+        viewBox="0 0 200 200"
+        width="200"
+      >
+        <title />
+        <path d="M114,100l49-49a9.9,9.9,0,0,0-14-14L100,86,51,37A9.9,9.9,0,0,0,37,51l49,49L37,149a9.9,9.9,0,0,0,14,14l49-49,49,49a9.9,9.9,0,0,0,14-14Z" />
+      </svg>
+      {children}
+    </div>
+  </div>
+);
+
 function App() {
   const [open, setOpen] = useState(false);
 
@@ -14,24 +33,11 @@ function App() {
       >
         ✨ Открыть окно
       </button>
-      {open && (
-        <div className="overlay">
-          <div className="modal">
-            <svg
-              onClick={() => {
-                setOpen(false);
-              }}
-              height="200"
-              viewBox="0 0 200 200"
-              width="200"
-            >
-              <title />
-              <path d="M114,100l49-49a9.9,9.9,0,0,0-14-14L100,86,51,37A9.9,9.9,0,0,0,37,51l49,49L37,149a9.9,9.9,0,0,0,14,14l49-49,49,49a9.9,9.9,0,0,0,14-14Z" />
-            </svg>
-            <img src="https://media2.giphy.com/media/xT0xeJpnrWC4XWblEk/giphy.gif" />
-          </div>
-        </div>
-      )}
+      <Modal open={open} setOpen={setOpen}>
+        <img src="https://media2.giphy.com/media/xT0xeJpnrWC4XWblEk/giphy.gif" />
+        <h3>Это модальное окно</h3>
+        <button>Кнопка</button>
+      </Modal>
     </div>
   );
 }
